6f98766fdfbe0c7c0350087f13dae25e9a3a4cd5
Saito Takuma <takuma.saitou1@gmail.com>
2014-02-17 17:03:12 +0900

    Levenshutein 距離の実装終了. 単純な実装. 英語のみ有効

--- main.go
+++ main.go
@@ -1,41 +1,36 @@
 
 package main
-import ("fmt")
+import ("fmt"; "core/std")
 
-type TrieTree struct {
-	parent *TrieTree
-	path rune
-	children map[rune]*TrieTree
-}
-var root TrieTree
-
-// Trie 木を作成
-func MakeTrie(tree *TrieTree, name string) {
-	if tree == nil {panic("error occured")}
-	if tree.children == nil {
-		tree.children = make(map[rune]*TrieTree)
-	}
-	if len(name) == 0 {
-		tree.children[TERMINAL] = nil
-	} else {
-		char, size := utf8.DecodeRuneInString(name)
-		_, ok := tree.children[char]
-		if !ok {
-			tree.children[char] = new(TrieTree)
-			tree.children[char].parent = tree
-			tree.children[char].path = char
-		}
-		MakeTrie(tree.children[char], name[size:])
-	}
-}
+var dp[][]int
 
 // 単純な Cost 距離
-func Cost(x string, y string) {
+func Cost(x string, y string) int {	
+	var cost int
+	dp := make([][]int, len(x) + 1)
+	for i, _ := range dp {dp[i] = make([]int, len(y) + 1)}
+	for i, _ := range x {dp[i][0] = i}
+	for i, _ := range y {dp[0][i] = i}
 	
+	for i := 1; i <= len(x); i++ {
+		for j := 1; j <= len(y); j++ {
+			if x[i - 1] == y[j - 1] {cost = 0} else {cost = 1}
+			dp[i][j] = std.Min(dp[i][j - 1] + 1, dp[i - 1][j] + 1, dp[i - 1][j - 1] + cost)
+		}
+	}
+	return dp[len(x)][len(y)]
+}
+
+func TestCostEnglish() {
+	fmt.Println(Cost("sitting", "kitten") == 3)
+	fmt.Println(Cost("abcabccdaba", "abracdabra") == 4)
+	fmt.Println(Cost(
+		"Good Evening World! This is very nice posts!",
+		"Hello World! This is amazing posts!") == 19)
 }
 
 func main() {
-	
+	TestCostEnglish()
 }
 
 
