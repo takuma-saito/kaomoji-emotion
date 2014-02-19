e38c2b93d12bf4814f33faa697727d28166edd70
Saito Takuma <takuma.saitou1@gmail.com>
2014-02-19 10:40:23 +0900

    【バグ修正】評価関数の演算順序が逆のため、正しい値を出さなかった。

--- main.go
+++ main.go
@@ -6,59 +6,67 @@ import (
 	"strings"
 	"strconv"
 	"core/std"
+	"core/bit"
 )
 
-const MAX_LOOP = 300
-const DELTA = 0.01
+const MAX_LOOP = 500
+const DELTA = 1.0
 const SEPARETOR = "$"
 
 type FaceVector map[rune]float64
+type Face []rune
 
-var charSet map[rune]int
-var charHistogram map[rune]int
-var chars []rune
-var	IdToFace [][]rune
+type LearningItems struct {
+	histogram map[rune]int    // 文字の頻度分布
+	faces []Face              // 顔文字集合
+	faceVectors []FaceVector  // 顔文字の特徴ベクトル
+	ans []int
+	weight FaceVector
+}
 
 // 日本語に変換
 var toJa = map[int]string{-1:"悲しい", 1:"楽しい"}
 
-type LearningItem struct {
-	input FaceVector
-	answer int
-}
-var learningItems []LearningItem
-
-// g(c)   = tf(c) * idf(c)
-// tf(c)  = 文字 C が入力テキスト中に出現する回数
-// idf(c) = log2(N / histogram(c))
-// histogram(c)   = 訓練データ中に文字 C が出現する頻度
-// N      = 学習データの総数
-func MakeFaceVector(face []rune) FaceVector {
-	vector := make(FaceVector)
-	tf := make(map[rune]int)
-	for _, char := range face {
-		tf[char] += 1
-	}
-	for _, char := range face {
-		histogram, ok := charHistogram[char];
-		if !ok {continue}
-		vector[char] = float64(tf[char]) *
-			math.Log2(float64(len(IdToFace)) / float64(histogram))
-	}
-	return vector
+func GetFacesFromFile(filename string) (faces []Face, ans[]int) {
+	linenum := 0
+	std.ReadFile(filename, func(line string) {
+		words := strings.Split(line, SEPARETOR)
+		linenum += 1
+		if len(words) != 2 {
+			panic(fmt.Sprintf("Informal Learning data: %d %v\n", linenum, words))}
+		x, err := strconv.Atoi(words[1])
+		if err != nil {
+			panic(fmt.Sprintf("can't convert string to int: %s\n", words[1]))}
+		faces = append(faces, Face(words[0]))
+		ans = append(ans, x)
+	})
+	return
 }
 
-func ShowVector(vector FaceVector, face []rune) {
+func ShowFaces(faces []Face) {
+	for _, face := range faces {
+		fmt.Println(string(face))
+	}
+}
+
+func ShowVector(vector FaceVector, face Face) {
 	fmt.Println(string(face))
 	for _, char := range face {
-		fmt.Printf("%s -> %lf\n", string(char), vector[char])
+		fmt.Printf("%s -> %3.1f\n", string(char), vector[char])
+	}
+	fmt.Println()
+}
+
+func ShowVectors(vectors []FaceVector, faces []Face) {
+	for i, vector := range vectors {
+		ShowVector(vector, faces[i])
 	}
 	fmt.Println()
 }
 
 func ShowWeight(weight FaceVector) {
 	for char, w := range weight {
-		fmt.Printf("%s -> %lf\n", string(char), w)
+		fmt.Printf("%s -> %3.1f\n", string(char), w)
 	}
 	fmt.Println()
 }
@@ -88,84 +96,100 @@ func InProduct(x, y FaceVector) float64 {
 	return sum
 }
 
-func ScalarTimes(a float64, x FaceVector) FaceVector {
-	for c, v := range x {
-		x[c] = v * a
+func ScalarTimes(a float64, vector FaceVector) FaceVector {
+	res := make(FaceVector)
+	for key, value := range vector {
+		res[key] = a * value
 	}
-	return x
+	return res
 }
 
 func Sign(x float64) int {
 	if x < 0 {return -1} else {return 1}
 }
 
-func EstimateWeight(learningItems []LearningItem) FaceVector {
-	weight := make(FaceVector)
-	for char, _ := range charHistogram {weight[char] = 0}
-	for i := 0; i < MAX_LOOP; i++ {
-		for j := 0; j < len(learningItems); j++ {
-			item := learningItems[j]
-			if item.answer != Sign(InProduct(item.input, weight)) {
-				weight = ScalarTimes(float64(item.answer) * DELTA, Add(weight, item.input))
+func Partition(n int) int {
+	return (1 + bit.Log2(n)) * 4
+}
+
+func (items *LearningItems) Predict(face string) int {
+	return Sign(InProduct(items.weight,
+		MakeFaceVector([]rune(face), items.histogram, len(items.faces))))
+}
+
+func MakeFaceVector(face []rune, histogram map[rune]int, N int) FaceVector {
+	vector := make(FaceVector)
+	tf := make(map[rune]int)
+	for _, char := range face {
+		tf[char] += 1
+	}
+	for _, char := range face {
+		count, ok := histogram[char];
+		if !ok {continue}
+		vector[char] = float64(tf[char]) * math.Log2(float64(N) / float64(count))
+	}
+	return vector
+}
+
+func MakeLItems(faces []Face, ans []int) *LearningItems {
+	items := &LearningItems{
+		histogram: make(map[rune]int),
+		faces: faces,
+		faceVectors: []FaceVector{},
+		ans: ans,
+	}
+	
+	// histogram の構成
+	for _, face := range faces {
+		exists := make(map[rune]bool)
+		for _, char := range []rune(face) {
+			if _, ok := items.histogram[char]; !ok {
+				items.histogram[char] = 1
+			} else {
+				if !exists[char] {
+					items.histogram[char] += 1
+					exists[char] = true
+				}
 			}
-			// ShowVector(ScalarTimes(item.answer, Add(weight, item.input)), IdToFace[j])
 		}
 	}
-	return weight
+	
+	for _, face := range items.faces {
+		items.faceVectors = append(items.faceVectors,
+			MakeFaceVector(face, items.histogram, len(items.faces)))
+	}
+	return items
 }
 
-func Init() {
-	chars = make([]rune, 0)
-	IdToFace = make([][]rune, 0)
-	charSet = make(map[rune]int)
-	charHistogram = make(map[rune]int)
-	learningItems = make([]LearningItem, 0)
+func (items *LearningItems) EstimateWeight() {
+	items.weight = make(FaceVector)
+	for char, _ := range items.histogram {items.weight[char] = 0}
+	for i := 0; i < MAX_LOOP; i++ {
+		for j := 0; j < len(items.faces); j++ {
+			if items.ans[j] != Sign(InProduct(items.faceVectors[j], items.weight)) {
+				items.weight = Add(items.weight,
+					ScalarTimes(float64(items.ans[j]) * DELTA, items.faceVectors[j]))
+			}
+		}
+	}
 }
 
-func Predict(face string, weight FaceVector) int {
-	return Sign(InProduct(weight, MakeFaceVector([]rune(face))))
+// 交差検定
+func CrossValidate(faces []Face, ans []int) {
+	k := Partition(len(faces)) // 分割数
+	for i := 1; i <= len(faces) / k; i++ {
+		// data := 
+	}
 }
 
 func main() {
-	Init()
-	ans := make([]int, 0)
-	linenum := 0
-	std.ReadFile("test/fun-sad-face.txt", func(line string) {
-		linenum++
-		words := strings.Split(line, SEPARETOR)
-		face := words[0]
-		if len(words) != 2 {
-			panic(fmt.Sprintf("Informal Learning data: %d %v\n", linenum, words))}
-		exists := make(map[rune]bool)
-		for _, char := range []rune(face) {
-			if _, ok := charHistogram[char]; ok && !exists[char] {
-				charHistogram[char] += 1
-				exists[char] = true
-			}
-			if _, ok := charSet[char]; !ok {
-				chars = append(chars, char)
-				charSet[char] = len(chars) - 1
-				charHistogram[char] = 1
-			}
-		}
-		IdToFace = append(IdToFace, []rune(face))
-		x, err := strconv.Atoi(words[1])
-		if err != nil {panic(fmt.Sprintf("can't convert string to int: %s\n", words[1]))}
-		ans = append(ans, x)
-	})
-	for i, face := range IdToFace {
-		vector := MakeFaceVector(face)
-		learningItems = append(learningItems,
-			LearningItem{input: vector, answer: ans[i]})
-	  // ShowVector(vector, face)
-	}
-	weight := EstimateWeight(learningItems)
-	
-	// 学習の判定結果を見る
+	faces, ans := GetFacesFromFile("test/fun-sad-face.txt")
+	items := MakeLItems(faces, ans)
+	items.EstimateWeight()
+	// ShowWeight(items.weight)
+	// ShowVectors(items.faceVectors, items.faces)
 	std.ReadFile("test/kaomoji-250.txt", func(face string) {
-		fmt.Printf("%s  ---> %s\n", string(face), toJa[Predict(face, weight)])
+		fmt.Printf("%-20s --> %s\n", string(face), toJa[items.Predict(face)])
 	})
 }
-
-
 
